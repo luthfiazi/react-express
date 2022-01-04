6deb6ed0ca6f90d5ba9ac1a6d41f8dbd999029fd
luthfiazi <luthfiazizi11@gmail.com>
2022-01-04 21:30:11 +0700

    add information server

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type', 'text/plain');
     res.write('Hello World!');
     res.end();
-}).listen(4000)
+});
+
+const hostname = '127.0.0.1'
+const port = 4000
+server.listen(port, hostname, ()=>{
+    console.log(`server listen at: ${hostname}:${port}/`);
+})
